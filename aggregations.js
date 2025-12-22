65ebc233be512a1cddf36f08c5e9c4c2f4d6e08f
Rawan-Alnaggary <148374317+RawanElnaggary@users.noreply.github.com>
2025-12-22 18:46:16 +0200

    finish aggregation

--- aggregations.js
+++ aggregations.js
@@ -1,5 +1,18 @@
 //1. Tickets sold per event
-
+db.bookings.aggregate([
+    { $unwind: "$tickets" },
+    {
+        $lookup: {
+        from: "events",
+        localField: "eventId",  // field from the bookings collection
+        foreignField: "_id", // field from the events collection
+        as: "event"
+        }
+    },
+    { $unwind: "$event" },
+    { $group: { _id: "$event.name", totalTicketsSold: { $sum: 1 } } },
+    { $project: { event: "$_id", totalTicketsSold: 1, _id: 0 } }
+]);
 
 
 //2. Revenue per event
@@ -20,7 +33,21 @@ db.bookings.aggregate([
 
 
 //3. Top users by number of bookings
-
+db.bookings.aggregate([
+    {
+        $lookup: {
+            from: "users",
+            localField: "userId",
+            foreignField: "_id",
+            as: "user"
+        }
+    },
+    { $unwind: "$user" },
+    { $group: { _id: "$user.name", totalBookings: { $sum: 1 } } },
+    { $sort: { totalBookings: -1}},
+    { $project: { User: "$_id", totalBookings: 1, _id: 0 } },
+    { $limit: 5}
+])
 
 
 //4. Event popularity ranking
